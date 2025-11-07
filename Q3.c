21692b197d3921d223b8765b15894db3fa1ff2e9
Sirat Shaikh <you@example.com>
2025-11-07 22:41:26 +0530

    first commit

---
+++ Q3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n, num, originalNum, remainder, digits;
+    double result;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Armstrong numbers between 1 and %d are:\n", n);
+
+    for(num = 1; num <= n; num++) {
+        originalNum = num;
+        digits = 0;
+        result = 0.0;
+
+        int temp = num;
+        while (temp != 0) {
+            temp /= 10;
+            digits++;
+        }
+
+        temp = num;
+        while (temp != 0) {
+            remainder = temp % 10;
+            result += pow(remainder, digits);
+            temp /= 10;
+        }
+
+        if ((int)result == originalNum)
+            printf("%d ", originalNum);
+    }
+
+    return 0;
+}
