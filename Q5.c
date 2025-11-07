83b306bcdffb78a630929bffb99e304f36b32feb
Sirat Shaikh <you@example.com>
2025-11-07 22:45:16 +0530

    first commit

---
+++ Q5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, sum;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Perfect numbers between 1 and %d are:\n", n);
+
+    for(i = 1; i <= n; i++) {
+        sum = 0;
+
+        for(j = 1; j < i; j++) {
+            if(i % j == 0)
+                sum += j;
+        }
+         
+        if(sum == i)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
